b360fc7f8c82849445dba0ef3c5e46a185313953
Maxime <blaessmaxime@gmail.com>
2023-11-08 12:27:10 +0100

    TP3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'TP3';
-}
+  import { Component, OnInit } from '@angular/core';
+  import { ProductService } from './product.service';
+  
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  export class AppComponent implements OnInit {
+    title = 'TP3';
+    productsList: any[] = [];
+  
+    constructor(private productService: ProductService) {}
+  
+    ngOnInit(): void {
+      this.productService.getProducts().subscribe(
+        (data: any[]) => {
+          this.productsList = data; // Initialise productsList avec les données récupérées
+        },
+        error => {
+          console.error('Error fetching products', error);
+        }
+      );
+    }
+  }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductSearchComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
